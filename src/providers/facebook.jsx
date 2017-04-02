c5c1cd8ecc81c31653157ba692fd83da65605e93
Chadi Kazan <kazan.chadi@gmail.com>
2017-04-02 22:55:18 +0200

    facebook login component

--- src/providers/facebook.jsx
+++ src/providers/facebook.jsx
@@ -1,58 +1,74 @@
 import React, { PureComponent } from 'react';
+import FacebookLogin from 'react-facebook-login';
 
 export default class Facebook extends PureComponent {
   constructor() {
     super();
+    let token;
+    try {
+      token = JSON.parse(localStorage.getItem('facebook_token'));
+      if (token) {
+        const fiveMinsAgo = new Date().getTime() / 1000 | 0 - 300;
+        if (fiveMinsAgo > token.expiresAt) {
+          token = null;
+        }
+      }
+    } catch (err) {
+      token = null;
+    }
+    
     this.state = {
-      initialized: false
+      token
     };
   }
-  componentDidMount() {
-    const self = this;
-    if (window.FB) {
-      this.setState({ initialized: true });
-    } else {
-      window.fbAsyncInit = function() {
-        window.FB.init({
-          appId      : '1904555776447565',
-          cookie     : true,  // enable cookies to allow the server to access the session
-          xfbml      : true,  // parse social plugins on this page
-          version    : 'v2.8'
-        });
-        self.setState({ initialized: true });
-      };
-
-      // pre load sdks
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_PI/sdk.js#xfbml=1&version=v2.8&appId=1904555776447565";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-sdk'));
-    }
- } 
- render() {
-   if (!this.state.initialized) {
-     return <div className="provider-nosession">Loading...</div>;
+  onFacebookResponse = response => {
+    if (response.accessToken) {
+      const expiresAt = (new Date().getTime() / 1000 | 0) + response.expiresIn;
+      const facebookToken = { ...response, expiresAt };
+      localStorage.setItem(`facebook_token`, JSON.stringify(facebookToken));
+      // TODO dispatch an action to the store. for now, for the sake of the demo just change the state
+      this.setState({ token: facebookToken });
    }
+  }
+  renderPageTitle(fbToken) {
+    return fbToken ? null : <li className="dbl-lst-margin bigger-text diluted-color">
+      <div>Import Liked</div>
+      <div>Facebook Music Pages</div>
+    </li>;
+  }
+  renderFBControl(fbToken) {
+    if (fbToken) { 
+      return <li><div><img src={fbToken.picture.data.url} /></div>
+        <div>{fbToken.name}</div></li>;
+    }
+    const fbButtonStyle = {
+      textTransform: 'none',
+      marginBottom: '10px', 
+      textShadow: '0 -1px 0 #354c8c',
+      backgroundImage: 'linear-gradient(rgb(76, 105, 186), rgb(59, 85, 160))'
+    };
+    return <li>
+      <FacebookLogin
+        appId="1904555776447565"
+        autoLoad={false}
+        fields="name,picture"
+        scope="user_actions.music"
+        icon="fa fa-facebook"
+        textButton="Connect with facebook"
+        size="small"
+        buttonStyle={fbButtonStyle}
+        callback={this.onFacebookResponse} />
+      <hr />
+    </li>;
+  }
+ render() {
+   const { token } = this.state;
    return (
      <div className="main-workarea">
      <ul className="list-unstyled provider-nosession text-center">
         <li className="dbl-lst-margin"><h2>Facebook</h2></li>
-        <li className="dbl-lst-margin bigger-text diluted-color">
-         <div>Import Liked</div>
-         <div>Facebook Music Pages</div>
-        </li>
-        <li>
-           <div className="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
-         </li>
-       <li>
-           <div>
-             <span className="secondary-text">Skip</span>
-           </div>
-          <hr />  
-        </li>
+        {this.renderPageTitle(token)}
+        {this.renderFBControl(token)}
         <li> 
             <div className="bigger-text"><span>Manually Add Facebook Page URL</span></div>
          </li>
